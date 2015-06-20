fa270901c6b71ab8ac03b2250525ecb102502369
Carlo Pacioni <carlo.pacioni@gmail.com>
2015-06-20 11:19:07 +0800

    plot for each locus with axis title and CI + drop out data from first negative/Inf std

--- R/BSP.R
+++ R/BSP.R
@@ -7,10 +7,9 @@ library(gridExtra)
 dir.in=sky.path
 skylinefile=NULL
 dir.out=NULL
-nloci=1
 all.loci=TRUE
 overall=TRUE
-param=1
+params=1
 save2disk=TRUE
 
 #' @param dir.in The local folder containing skylinefile files (default: NULL)
@@ -46,15 +45,28 @@ BSP <- function(dir.in=NULL, skylinefile=NULL, dir.out=NULL,
     ggplot(, aesDT(...))
   }
   ##############################################
-  plot.locus <- function(i, dpar){
-    p <- dpar[J(i), ggplotDT(x=Age, y=Parameter.value)+geom_line()]
+  plot.locus <- function(i, dpar, par){
+    dlocus <- dpar[J(i), ]
+    neg <- dlocus[, which(Standard.deviation < 0)[1]]
+    infin <- dlocus[, which(Standard.deviation == Inf)[1]]
+    sel.min <- suppressWarnings(min(neg, infin, na.rm = TRUE))
+    if(sel.min == Inf) {sel.min <- dlocus[, length(Bin)]
+        } else{
+          sel.min <- sel.min - 1
+        }
+    p <- dlocus[1:sel.min, ggplotDT(x=Age, y=Parameter.value) + 
+                  geom_line() +
+                  geom_ribbon(data=dlocus[1:sel.min, ],  alpha=0.2,
+                                aes(x=Age, ymax=Upper, ymin=Lower)) +
+                  theme_classic() + 
+                  ylab(paste("Parameter", par)) + xlab("Time")]
     return(p)
   }
   
   plot.loci <- function(par, data, loci){
     dpar <- data[J(par), ]
     setkey(dpar, Locus)
-    p.loci <- lapply(loci, plot.locus, dpar)
+    p.loci <- lapply(loci, plot.locus, dpar, par)
     plots <- do.call(arrangeGrob,  p.loci)
     return(plots)
   }
@@ -85,19 +97,18 @@ if(is.null(dir.out)) dir.out <- dir.in
 
 rl <- readLines(paste0(dir.in, "/", "skylinefile"))
 lstart <- grep(rl, pattern = "^1\t1\t")[1]
+h <- c("Locus", "Parameter-number", "Bin", "Age", "Parameter-value", 
+               "Parameter-Frequency", "Standard-deviation", "Counts-per-bin", 
+               "Autocorrelation-per-bin")
 d<-data.table((read.table(paste0(dir.in, "/", "skylinefile"), 
                skip=lstart - 1, header = F, col.names = h)))
 nloci <- length(d[, unique(Locus)])
 if(nloci > 1) nloci <- nloci - 1
-
-if(all.loci == TRUE){
-  setkey(d, Parameter.number)
-  
-}
+setkey(d, Parameter.number)
+d[, Standard.error := Standard.deviation / sqrt(Counts.per.bin)]
+d[, Upper := Parameter.value + 1.96 * Standard.error]
+d[, Lower := Parameter.value - 1.96 * Standard.error]  
 
 lplots.byparams <- lapply(params, plot.loci, data=d, loci=1:nloci)
 
-neg <- dloci[, which(Standard.deviation < 0)[1], by="Locus"]
-infin <- dloci[, which(Standard.deviation == Inf)[1], by="Locus"]
-sel.min <- min(c(neg[, V1], infin[, V1]), na.rm = TRUE)
 
